e67f5ba667462e91904b993650d82e9b1ea8a864
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2017-08-17 00:55:04 -0700

    Define constants and typedefs in common.h and common.cpp

--- C++/common.cpp
+++ C++/common.cpp
@@ -1,5 +1,11 @@
 #include "common.h"
 
 namespace Godzilla {
+	double VEL_MIN_TOL = 1e-6;
+	double VEL_MAX_TOL = 1e6;
+	double GRID_MIN_SIZE = 1e-6;
+	double GRID_MAX_SIZE = 1e6;
 
+	// For lock manager
+	int VELOCITY1D_DATA_ID = 1;
 }
--- C++/common.h
+++ C++/common.h
@@ -1,6 +1,17 @@
 #pragma once
 #include <vector>
+#include <complex>
 
 namespace Godzilla {
+	typedef std::complex<double> xd;
+	typedef std::vector<double> vecd;
+	typedef std::vector<std::complex<double>> vecxd;
 
+	extern double VEL_MIN_TOL;
+	extern double VEL_MAX_TOL;
+	extern double GRID_MIN_SIZE;
+	extern double GRID_MAX_SIZE;
+
+	// For lock manager
+	extern int VELOCITY1D_DATA_ID;
 }
