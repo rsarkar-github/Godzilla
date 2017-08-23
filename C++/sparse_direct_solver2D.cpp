ba2bdea48879bf947a4ebe85a67ae74bc015402a
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2017-08-23 03:17:07 -0700

    Updates on solver

--- C++/sparse_direct_solver2D.cpp
+++ C++/sparse_direct_solver2D.cpp
@@ -1,19 +1,110 @@
 #include "sparse_direct_solver2D.h"
+#include <cmath>
 
 namespace Godzilla {
 	namespace Helmholtz2DReal {
 		// Constructors
-		SparseDirectSolver2D::SparseDirectSolver2D() {
+		SparseDirectSolver2D::SparseDirectSolver2D(const Godzilla::Velocity2D &vel2D, const Godzilla::Field2D &forcing2D,
+												   const Godzilla::BoundaryCondition2D &bc2D, const double &omega)
+			: _vel2D(nullptr), _forcing2D(nullptr), _bc2D(nullptr), _omega(omega), _initialized_state(false), _is_matrix_ready(false), _is_rhs_ready(false) {
 
+			const Godzilla::Geometry2D &geom2D = vel2D.get_geom2D();
+			if (this->is_velocity_real(vel2D) && geom2D.is_equal(forcing2D.get_geom2D()) && geom2D.is_equal(bc2D.get_geom2D())) {
+				_vel2D = &vel2D;
+				_forcing2D = &forcing2D;
+				_bc2D = &bc2D;
+				_sim_geom2D = this->create_sim_geom2D(*_bc2D);
+				_pad1 = this->calculate_pad1(*_bc2D);
+				_pad2 = this->calculate_pad2(*_bc2D);
+				_pad3 = this->calculate_pad3(*_bc2D);
+				_pad4 = this->calculate_pad4(*_bc2D);
+				_sim_bc_face1 = this->calculate_sim_bc_face1(*_bc2D, _sim_geom2D, _pad4, _pad2);
+				_sim_bc_face2 = this->calculate_sim_bc_face2(*_bc2D, _sim_geom2D, _pad1, _pad3);
+				_sim_bc_face3 = this->calculate_sim_bc_face3(*_bc2D, _sim_geom2D, _pad4, _pad2);
+				_sim_bc_face4 = this->calculate_sim_bc_face4(*_bc2D, _sim_geom2D, _pad1, _pad3);
+				_velocity_data_simgrid = this->velocity_simgrid_extrap(*_vel2D, _sim_geom2D, _pad1, _pad2, _pad3, _pad4);
+				_forcing_data_simgrid = this->field_simgrid_zeropad(*_forcing2D, _sim_geom2D, _pad1, _pad4);
+				_initialized_state = true;
+			}
+			else {
+				std::cerr << "Velocity not real / Geometries did not match. Initialization failed." << std::endl;
+			}
 		}
 
 		// Public methods
+		void SparseDirectSolver2D::change_velocity_data(const Godzilla::Velocity2D &vel2D) {
+			if (_vel2D->get_geom2D().is_equal(vel2D.get_geom2D()) && this->is_velocity_real(vel2D)) {
+				_vel2D = &vel2D;
+				_velocity_data_simgrid = this->velocity_simgrid_extrap(*_vel2D, _sim_geom2D, _pad1, _pad2, _pad3, _pad4);
+				_is_matrix_ready = false;
+				_is_rhs_ready = false;
+			}
+			else {
+				std::cerr << "Input geometry does not match existing geometry. Cannot change velocity data." << std::endl;
+			}
+		}
+
+		void SparseDirectSolver2D::change_forcing_data(const Godzilla::Field2D &forcing2D) {
+			if (_vel2D->get_geom2D().is_equal(forcing2D.get_geom2D())) {
+				_forcing2D = &forcing2D;
+				_forcing_data_simgrid = this->field_simgrid_zeropad(*_forcing2D, _sim_geom2D, _pad1, _pad4);
+				_is_matrix_ready = false;
+				_is_rhs_ready = false;
+			}
+			else {
+				std::cerr << "Input geometry does not match existing geometry. Cannot change forcing data." << std::endl;
+			}
+		}
+
+		void SparseDirectSolver2D::change_boundary_conditions(const Godzilla::BoundaryCondition2D &bc2D) {
+			if (_vel2D->get_geom2D().is_equal(bc2D.get_geom2D())) {
+				_bc2D = &bc2D;
+				_sim_geom2D = this->create_sim_geom2D(*_bc2D);
+				_pad1 = this->calculate_pad1(*_bc2D);
+				_pad2 = this->calculate_pad2(*_bc2D);
+				_pad3 = this->calculate_pad3(*_bc2D);
+				_pad4 = this->calculate_pad4(*_bc2D);
+				_sim_bc_face1 = this->calculate_sim_bc_face1(*_bc2D, _sim_geom2D, _pad4, _pad2);
+				_sim_bc_face2 = this->calculate_sim_bc_face2(*_bc2D, _sim_geom2D, _pad1, _pad3);
+				_sim_bc_face3 = this->calculate_sim_bc_face3(*_bc2D, _sim_geom2D, _pad4, _pad2);
+				_sim_bc_face4 = this->calculate_sim_bc_face4(*_bc2D, _sim_geom2D, _pad1, _pad3);
+				_velocity_data_simgrid = this->velocity_simgrid_extrap(*_vel2D, _sim_geom2D, _pad1, _pad2, _pad3, _pad4);
+				_forcing_data_simgrid = this->field_simgrid_zeropad(*_forcing2D, _sim_geom2D, _pad1, _pad4);
+				_is_matrix_ready = false;
+				_is_rhs_ready = false;
+			}
+			else {
+				std::cerr << "Input geometry does not match existing geometry. Cannot change boundary conditions." << std::endl;
+			}
+		}
+
+		void SparseDirectSolver2D::change_omega(const double &omega) {
+			_omega = omega;
+			_is_matrix_ready = false;
+		}
+
+		void SparseDirectSolver2D::create_sparse_matrix() {
+
+		}
 
 		// Private methods
+		bool SparseDirectSolver2D::is_velocity_real(const Godzilla::Velocity2D &vel2D_in) const {
+			bool flag = true;
+			const size_t nelem = vel2D_in.get_nelem();
+			const Godzilla::xd *ptr_data = vel2D_in.get_cdata().data();
+			for (size_t i = 0; i < nelem; ++i) {
+				if (ptr_data[i].imag() != 0) {
+					flag = false;
+					break;
+				}
+			}
+			return flag;
+		}
+
 		Godzilla::Geometry2D SparseDirectSolver2D::create_sim_geom2D(const Godzilla::BoundaryCondition2D &bc2D) const {
 			
 			// Get geometry
-			Godzilla::Geometry2D &geom2D = bc2D.get_geom2D();
+			const Godzilla::Geometry2D &geom2D = bc2D.get_geom2D();
 
 			// Default n1, n2, n3, n4 for NBC, DBC
 			size_t n1 = 0;
@@ -26,10 +117,10 @@ namespace Godzilla {
 			if (bc2D.get_bc_face3() == "PML") n3 = bc2D.get_pmlcells_face3();
 			if (bc2D.get_bc_face4() == "PML") n4 = bc2D.get_pmlcells_face4();
 
-			size_t nX = geom2D.get_nX() + n1 + n3;
-			size_t nY = geom2D.get_nY() + n2 + n4;
-			double startX = geom2D.get_startX() - n1 * geom2D.get_hX();
-			double startY = geom2D.get_startY() - n4 * geom2D.get_hY();
+			const size_t nX = geom2D.get_nX() + n1 + n3;
+			const size_t nY = geom2D.get_nY() + n2 + n4;
+			const double startX = geom2D.get_startX() - n1 * geom2D.get_hX();
+			const double startY = geom2D.get_startY() - n4 * geom2D.get_hY();
 
 			Godzilla::Geometry2D sim_geom2D(geom2D);
 			sim_geom2D.set_geometry2D(nX, startX, geom2D.get_hX(), nY, startY, geom2D.get_hY());
@@ -75,12 +166,12 @@ namespace Godzilla {
 		Godzilla::vecxd SparseDirectSolver2D::field_simgrid_zeropad(const Godzilla::Field2D &field2D_in, const Godzilla::Geometry2D &geom2D_out,
 																	const size_t &pad1, const size_t &pad4) const {
 
-			size_t nX_out = geom2D_out.get_nX();
-			size_t nY_out = geom2D_out.get_nY();
+			const size_t nX_out = geom2D_out.get_nX();
+			const size_t nY_out = geom2D_out.get_nY();
 			Godzilla::vecxd out(nX_out * nY_out, 0.);
 
-			size_t nX_in = field2D_in.get_geom2D().get_nX();
-			size_t nY_in = field2D_in.get_geom2D().get_nY();
+			const size_t nX_in = field2D_in.get_geom2D().get_nX();
+			const size_t nY_in = field2D_in.get_geom2D().get_nY();
 
 			const Godzilla::xd *ptr_in = field2D_in.get_cdata().data();
 			Godzilla::xd *ptr_out = out.data();
@@ -100,12 +191,12 @@ namespace Godzilla {
 		Godzilla::vecxd SparseDirectSolver2D::velocity_simgrid_extrap(const Godzilla::Velocity2D &vel2D_in, const Godzilla::Geometry2D &geom2D_out,
 																	  const size_t &pad1, const size_t &pad2, const size_t &pad3, const size_t &pad4) const {
 
-			size_t nX_out = geom2D_out.get_nX();
-			size_t nY_out = geom2D_out.get_nY();
+			const size_t nX_out = geom2D_out.get_nX();
+			const size_t nY_out = geom2D_out.get_nY();
 			Godzilla::vecxd out(nX_out * nY_out, 0.);
 
-			size_t nX_in = vel2D_in.get_geom2D().get_nX();
-			size_t nY_in = vel2D_in.get_geom2D().get_nY();
+			const size_t nX_in = vel2D_in.get_geom2D().get_nX();
+			const size_t nY_in = vel2D_in.get_geom2D().get_nY();
 
 			const Godzilla::xd *ptr_in = vel2D_in.get_cdata().data();
 			Godzilla::xd *ptr_out = out.data();
@@ -161,25 +252,26 @@ namespace Godzilla {
 			                                                         const size_t &pad4, const size_t &pad2) const {
 			
 			const Godzilla::xd *ptr_in = bc2D.get_data1().data();
-			size_t nY_in = bc2D.get_geom2D().get_nY();
+			const size_t nY_in = bc2D.get_geom2D().get_nY();
 
 			Godzilla::vecxd out(geom2D_out.get_nY(), 0.);
-			Godzilla::xd *ptr_out = out.data();
 
-			for (size_t i = 0; i < pad4; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
+			if (bc2D.get_bc_face1() != "PML") {
+				Godzilla::xd *ptr_out = out.data();
+				for (size_t i = 0; i < pad4; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
+				for (size_t i = 0; i < nY_in; ++i) {
+					*ptr_out = ptr_in[i];
+					++ptr_out;
+				}
+				ptr_in += nY_in - 1;
+				for (size_t i = 0; i < pad2; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
 			}
-			for (size_t i = 0; i < nY_in; ++i) {
-				*ptr_out = ptr_in[i];
-				++ptr_out;
-			}
-			ptr_in += nY_in - 1;
-			for (size_t i = 0; i < pad2; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
-			}
-
 			return out;
 		}
 
@@ -187,25 +279,26 @@ namespace Godzilla {
 																	 const size_t &pad1, const size_t &pad3) const {
 
 			const Godzilla::xd *ptr_in = bc2D.get_data2().data();
-			size_t nX_in = bc2D.get_geom2D().get_nX();
+			const size_t nX_in = bc2D.get_geom2D().get_nX();
 
 			Godzilla::vecxd out(geom2D_out.get_nX(), 0.);
-			Godzilla::xd *ptr_out = out.data();
 
-			for (size_t i = 0; i < pad1; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
+			if (bc2D.get_bc_face2() != "PML") {
+				Godzilla::xd *ptr_out = out.data();
+				for (size_t i = 0; i < pad1; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
+				for (size_t i = 0; i < nX_in; ++i) {
+					*ptr_out = ptr_in[i];
+					++ptr_out;
+				}
+				ptr_in += nX_in - 1;
+				for (size_t i = 0; i < pad3; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
 			}
-			for (size_t i = 0; i < nX_in; ++i) {
-				*ptr_out = ptr_in[i];
-				++ptr_out;
-			}
-			ptr_in += nX_in - 1;
-			for (size_t i = 0; i < pad3; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
-			}
-
 			return out;
 		}
 
@@ -213,25 +306,26 @@ namespace Godzilla {
 																	 const size_t &pad4, const size_t &pad2) const {
 
 			const Godzilla::xd *ptr_in = bc2D.get_data3().data();
-			size_t nY_in = bc2D.get_geom2D().get_nY();
+			const size_t nY_in = bc2D.get_geom2D().get_nY();
 
 			Godzilla::vecxd out(geom2D_out.get_nY(), 0.);
-			Godzilla::xd *ptr_out = out.data();
 
-			for (size_t i = 0; i < pad4; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
+			if (bc2D.get_bc_face3() != "PML") {
+				Godzilla::xd *ptr_out = out.data();
+				for (size_t i = 0; i < pad4; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
+				for (size_t i = 0; i < nY_in; ++i) {
+					*ptr_out = ptr_in[i];
+					++ptr_out;
+				}
+				ptr_in += nY_in - 1;
+				for (size_t i = 0; i < pad2; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
 			}
-			for (size_t i = 0; i < nY_in; ++i) {
-				*ptr_out = ptr_in[i];
-				++ptr_out;
-			}
-			ptr_in += nY_in - 1;
-			for (size_t i = 0; i < pad2; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
-			}
-
 			return out;
 		}
 
@@ -239,26 +333,132 @@ namespace Godzilla {
 																	 const size_t &pad1, const size_t &pad3) const {
 
 			const Godzilla::xd *ptr_in = bc2D.get_data4().data();
-			size_t nX_in = bc2D.get_geom2D().get_nX();
+			const size_t nX_in = bc2D.get_geom2D().get_nX();
 
 			Godzilla::vecxd out(geom2D_out.get_nX(), 0.);
-			Godzilla::xd *ptr_out = out.data();
 
-			for (size_t i = 0; i < pad1; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
+			if (bc2D.get_bc_face4() != "PML") {
+				Godzilla::xd *ptr_out = out.data();
+				for (size_t i = 0; i < pad1; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
+				for (size_t i = 0; i < nX_in; ++i) {
+					*ptr_out = ptr_in[i];
+					++ptr_out;
+				}
+				ptr_in += nX_in - 1;
+				for (size_t i = 0; i < pad3; ++i) {
+					*ptr_out = *ptr_in;
+					++ptr_out;
+				}
 			}
-			for (size_t i = 0; i < nX_in; ++i) {
-				*ptr_out = ptr_in[i];
-				++ptr_out;
-			}
-			ptr_in += nX_in - 1;
-			for (size_t i = 0; i < pad3; ++i) {
-				*ptr_out = *ptr_in;
-				++ptr_out;
-			}
-
 			return out;
 		}
+
+		Godzilla::vecxd SparseDirectSolver2D::calculate_sX(const Godzilla::vecd &x, const Godzilla::Geometry2D &geom2D, const double &omega,
+														   const size_t &pad1, const size_t &pad3) const {
+			const double startX = geom2D.get_startX();
+			const double endX = geom2D.get_endX();
+			const double start_bcX = startX + geom2D.get_hX() * pad1;
+			const double end_bcX = endX - geom2D.get_hX() * pad3;
+			const double pmlwidth_face1 = start_bcX - startX;
+			const double pmlwidth_face3 = endX - end_bcX;
+
+			const size_t nelem = x.size();
+			const double *ptr_in = x.data();
+			Godzilla::vecxd out(nelem, 0.);
+			Godzilla::xd * ptr_out = out.data();
+
+			const Godzilla::xd j(0., 1.);
+			const Godzilla::xd j1(1., 0.);
+
+			if (geom2D.get_hX() > 0) {
+				double sigmaX = 0.;
+				for (size_t i = 0; i < nelem; ++i) {
+					if ((ptr_in[i] >= start_bcX) && (ptr_in[i] <= end_bcX)) {
+						sigmaX = 0.;
+					}
+					else if (ptr_in[i] < start_bcX) {
+						sigmaX = std::pow((ptr_in[i] - start_bcX) / pmlwidth_face1, 2.0);
+					}
+					else if (ptr_in[i] > end_bcX) {
+						sigmaX = std::pow((ptr_in[i] - end_bcX) / pmlwidth_face3, 2.0);
+					}
+					sigmaX /= omega;
+					ptr_out[i] = j1 / (j1 + sigmaX * j);
+				}
+			}
+			else {
+				double sigmaX = 0.;
+				for (size_t i = 0; i < nelem; ++i) {
+					if ((ptr_in[i] <= start_bcX) && (ptr_in[i] >= end_bcX)) {
+						sigmaX = 0.;
+					}
+					else if (ptr_in[i] > start_bcX) {
+						sigmaX = std::pow((ptr_in[i] - start_bcX) / pmlwidth_face1, 2.0);
+					}
+					else if (ptr_in[i] < end_bcX) {
+						sigmaX = std::pow((ptr_in[i] - end_bcX) / pmlwidth_face3, 2.0);
+					}
+					sigmaX /= omega;
+					ptr_out[i] = j1 / (j1 + sigmaX * j);
+				}
+			}
+			return out;
+		}
+
+		Godzilla::vecxd SparseDirectSolver2D::calculate_sY(const Godzilla::vecd &y, const Godzilla::Geometry2D &geom2D, const double &omega,
+														   const size_t &pad4, const size_t &pad2) const {
+			const double startY = geom2D.get_startY();
+			const double endY = geom2D.get_endY();
+			const double start_bcY = startY + geom2D.get_hY() * pad4;
+			const double end_bcY = endY - geom2D.get_hY() * pad2;
+			const double pmlwidth_face4 = start_bcY - startY;
+			const double pmlwidth_face2 = endY - end_bcY;
+
+			const size_t nelem = y.size();
+			const double *ptr_in = y.data();
+			Godzilla::vecxd out(nelem, 0.);
+			Godzilla::xd * ptr_out = out.data();
+
+			const Godzilla::xd j(0., 1.);
+			const Godzilla::xd j1(1., 0.);
+
+			if (geom2D.get_hY() > 0) {
+				double sigmaY = 0.;
+				for (size_t i = 0; i < nelem; ++i) {
+					if ((ptr_in[i] >= start_bcY) && (ptr_in[i] <= end_bcY)) {
+						sigmaY = 0.;
+					}
+					else if (ptr_in[i] < start_bcY) {
+						sigmaY = std::pow((ptr_in[i] - start_bcY) / pmlwidth_face4, 2.0);
+					}
+					else if (ptr_in[i] > end_bcY) {
+						sigmaY = std::pow((ptr_in[i] - end_bcY) / pmlwidth_face2, 2.0);
+					}
+					sigmaY /= omega;
+					ptr_out[i] = j1 / (j1 + sigmaY * j);
+				}
+			}
+			else {
+				double sigmaY = 0.;
+				for (size_t i = 0; i < nelem; ++i) {
+					if ((ptr_in[i] <= start_bcY) && (ptr_in[i] >= end_bcY)) {
+						sigmaY = 0.;
+					}
+					else if (ptr_in[i] > start_bcY) {
+						sigmaY = std::pow((ptr_in[i] - start_bcY) / pmlwidth_face4, 2.0);
+					}
+					else if (ptr_in[i] < end_bcY) {
+						sigmaY = std::pow((ptr_in[i] - end_bcY) / pmlwidth_face2, 2.0);
+					}
+					sigmaY /= omega;
+					ptr_out[i] = j1 / (j1 + sigmaY * j);
+				}
+			}
+			return out;
+		}
+
 	}
 }
--- C++/sparse_direct_solver2D.h
+++ C++/sparse_direct_solver2D.h
@@ -2,24 +2,40 @@
 #include "boundary_condition2D.h"
 #include "velocity2D.h"
 #include "field2D.h"
+#include "umfpack.h"
 
 namespace Godzilla {
 	namespace Helmholtz2DReal {
 		class SparseDirectSolver2D {
 			public:
 				// Constructors
-				SparseDirectSolver2D();
+				SparseDirectSolver2D() = delete;
+				SparseDirectSolver2D(const Godzilla::Velocity2D &vel2D, const Godzilla::Field2D &forcing2D,
+									 const Godzilla::BoundaryCondition2D &bc2D, const double &omega);
 
 				// Public methods
+				void change_velocity_data(const Godzilla::Velocity2D &vel2D);
+				void change_forcing_data(const Godzilla::Field2D &forcing2D);
+				void change_boundary_conditions(const Godzilla::BoundaryCondition2D &bc2D);
+				void change_omega(const double &omega);
+				void create_sparse_matrix();
 
 			private:
 				// Private members
 				// Only the following are user dependent
-				Godzilla::Velocity2D _vel2D;
-				Godzilla::Field2D _forcing2D;
-				Godzilla::BoundaryCondition2D _bc2D;
+				const Godzilla::Velocity2D *_vel2D;
+				const Godzilla::Field2D *_forcing2D;
+				const Godzilla::BoundaryCondition2D *_bc2D;
+				double _omega;
 
 				// The rest are initialized from the user inputs
+				// If initialization was successful
+				bool _initialized_state;
+
+				// If solver is ready to solve
+				bool _is_matrix_ready;
+				bool _is_rhs_ready;
+
 				// Geometry for simulation
 				Godzilla::Geometry2D _sim_geom2D;
 
@@ -37,7 +53,14 @@ namespace Godzilla {
 				Godzilla::vecxd _sim_bc_face3;
 				Godzilla::vecxd _sim_bc_face4;
 
+				// Velocity and forcing data extrapolated and zeropadded to siimulation grid respectively
+				Godzilla::vecxd _velocity_data_simgrid;
+				Godzilla::vecxd _forcing_data_simgrid;
+
 				// Private methods
+				bool is_velocity_real(const Godzilla::Velocity2D &vel2D_in) const;
+				bool is_solver_ready() const { return (_is_matrix_ready && _is_rhs_ready); }
+
 				size_t calculate_pad1(const Godzilla::BoundaryCondition2D &bc2D) const;
 				size_t calculate_pad2(const Godzilla::BoundaryCondition2D &bc2D) const;
 				size_t calculate_pad3(const Godzilla::BoundaryCondition2D &bc2D) const;
@@ -58,6 +81,11 @@ namespace Godzilla {
 													   const size_t &pad4, const size_t &pad2) const;
 				Godzilla::vecxd calculate_sim_bc_face4(const Godzilla::BoundaryCondition2D &bc2D, const Godzilla::Geometry2D &geom2D_out,
 													   const size_t &pad1, const size_t &pad3) const;
+
+				Godzilla::vecxd calculate_sX(const Godzilla::vecd &x, const Godzilla::Geometry2D &geom2D, const double &omega,
+											 const size_t &pad1, const size_t &pad3) const;
+				Godzilla::vecxd calculate_sY(const Godzilla::vecd &y, const Godzilla::Geometry2D &geom2D, const double &omega,
+											 const size_t &pad4, const size_t &pad2) const;
 		};
 	}
 }
