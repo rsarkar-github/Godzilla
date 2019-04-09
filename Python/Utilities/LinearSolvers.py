36d9afda25342e8d08a7800b8660e764725e0c5a
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2019-04-09 03:21:32 -0700

    Bug fix tests

--- Python/Utilities/LinearSolvers.py
+++ Python/Utilities/LinearSolvers.py
@@ -104,6 +104,87 @@ def conjugate_gradient(linear_operator, rhs, x0, niter, printobj=False):
     return x, residual
 
 
+def conjugate_gradients(linear_operator, rhs, x0, niter, printobj=False):
+
+    TypeChecker.check_int_positive(x=niter)
+
+    # Get rhs norm
+    fac = np.linalg.norm(x=rhs)
+    print("Norm of rhs = ", fac)
+    if fac < 1e-15:
+        raise ValueError("Norm of rhs < 1e-15. Trivial solution of zero. Scale up the problem.")
+
+    # Scale rhs
+    rhs_new = rhs / fac
+    x = x0 / fac
+
+    # Calculate initial residual, and residual norm
+    r = rhs_new - linear_operator(x)
+    r_norm = np.linalg.norm(x=r)
+    if r_norm < 1e-12:
+        return x0, [r_norm]
+    r_norm_sq = r_norm ** 2
+
+    # Initialize p
+    p = copy.deepcopy(r)
+
+    # Initialize residual array, iteration array
+    residual = [r_norm]
+    if printobj:
+        objective = [np.real(0.5 * np.vdot(x, linear_operator(x)) - np.vdot(x, rhs_new))]
+
+    # Run CG iterations
+    for num_iter in range(niter):
+
+        t1 = time.time()
+
+        if printobj:
+            print(
+                "Beginning iteration : ", num_iter,
+                " , Residual : ", residual[num_iter],
+                " , Objective : ", objective[num_iter]
+            )
+        else:
+            print(
+                "Beginning iteration : ", num_iter,
+                " , Residual : ", residual[num_iter]
+            )
+
+        # Compute A*p and alpha
+        matrix_times_p = linear_operator(p)
+        alpha = r_norm_sq / np.vdot(p, matrix_times_p)
+
+        # Update x0, residual
+        x += alpha * p
+        r -= alpha * matrix_times_p
+
+        # Calculate beta
+        r_norm_new = np.linalg.norm(x=r)
+        r_norm_new_sq = r_norm_new ** 2
+        beta = r_norm_new_sq / r_norm_sq
+
+        # Check convergence
+        if r_norm_new < 1e-12:
+            break
+
+        # Update p, residual norm
+        p = r + beta * p
+        r_norm_sq = r_norm_new_sq
+
+        # Update residual array, iteration array
+        residual.append(r_norm_new)
+        if printobj:
+            objective.append(np.real(0.5 * np.vdot(x, linear_operator(x)) - np.vdot(x, rhs_new)))
+
+        t2 = time.time()
+        print("Iteration took ", t2 - t1, " s\n")
+
+    # Remove the effect of the scaling
+    x = x * fac
+
+    return x, residual
+
+
 if __name__ == "__main__":
 
     # Create a trial problem
@@ -116,8 +197,7 @@ if __name__ == "__main__":
 
     mat = np.dot(np.diag(d), q)
     mat = np.dot(np.conjugate(np.transpose(q)), mat)
-    vector_true = np.random.rand(dim).astype(dtype=np.complex64) \
-                  + 1j * np.random.rand(dim).astype(dtype=np.complex64)
+    vector_true = np.random.rand(dim).astype(dtype=np.complex64) + 1j * np.random.rand(dim).astype(dtype=np.complex64)
     b = np.dot(mat, vector_true)
     x0 = np.zeros((dim,), dtype=np.complex64)
 
