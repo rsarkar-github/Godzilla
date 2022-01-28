8196fc97ea5d4d49698419b6ab713d6ab115ea1a
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2022-01-28 14:18:26 -0800

    Some updates

--- Python/IntegralEquation/Scripts/scipy_vs_numpy_fft_comparison.py
+++ Python/IntegralEquation/Scripts/scipy_vs_numpy_fft_comparison.py
@@ -2,22 +2,49 @@ import numpy as np
 import scipy as sc
 import time
 
-n = 2000
-precision = np.complex64
-ntimes = 20
+n = 4000
+ntimes = 50
 
-a = np.zeros(shape=(n, n), dtype=np.complex64)
+print("-------------------------------------------------")
+print("Datatype: complex 64-bit tests")
+print("-------------------------------------------------")
+print("\n")
+
+precision = np.complex64
+
+a = np.zeros(shape=(n, n), dtype=precision)
 
 t1 = time.time()
 for _ in range(ntimes):
-    b = np.fft.fftn(a, axes=(0, 1))
+    np.fft.fftn(a, axes=(0, 1))
 t2= time.time()
 print("Average time taken (numpy) = ", (t2-t1) / ntimes, " s")
 
 t1 = time.time()
 for _ in range(ntimes):
-    b = sc.fft.fftn(a, axes=(0, 1))
+    sc.fft.fftn(a, axes=(0, 1))
 t2= time.time()
 print("Average time taken (scipy) = ", (t2-t1) / ntimes, " s")
 
 
+print("\n\n")
+print("-------------------------------------------------")
+print("Datatype: complex 128-bit tests")
+print("-------------------------------------------------")
+print("\n")
+
+precision = np.complex128
+
+a = np.zeros(shape=(n, n), dtype=precision)
+
+t1 = time.time()
+for _ in range(ntimes):
+    np.fft.fftn(a, axes=(0, 1))
+t2= time.time()
+print("Average time taken (numpy) = ", (t2-t1) / ntimes, " s")
+
+t1 = time.time()
+for _ in range(ntimes):
+    sc.fft.fftn(a, axes=(0, 1))
+t2= time.time()
+print("Average time taken (scipy) = ", (t2-t1) / ntimes, " s")
