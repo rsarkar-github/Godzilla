d8cd266ebe18a2f25c640d7d67687c834e3e7ed0
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2022-04-26 14:16:45 -0700

    Bash Script added

--- Python/IntegralEquation/BashScripts/lse_helmholtz_comp_2d.sh
+++ Python/IntegralEquation/BashScripts/lse_helmholtz_comp_2d.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/bash
-
-bash
-conda activate py38
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run1 0 100 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run2 0 10 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run3 0 50 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run4 1 50 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run5 2 50 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run6 11 50 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run7 -1 50 30
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run8 -1 50 10
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run9 -1 50 5
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run10 1 50 10
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run11 1 50 15
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run12 1 50 20
-
-python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run13 11 50 10
+if [ $1 -eq 0 ]
+then
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run1 0 100 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run2 0 10 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run3 0 50 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run4 1 50 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run5 2 50 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run6 11 50 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run7 -1 50 30
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run8 -1 50 10
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run9 -1 50 5
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run10 1 50 10
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run11 1 50 15
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run12 1 50 20
+    python -m Python.IntegralEquation.Scripts.lse_helmholtz_comp_2d run13 11 50 10
+fi
+if [ $1 -eq 1 ]
+then
+    str1="Python/IntegralEquation/Runs/lse_helmholtz_comp_2d/run"
+    str2="/Data/green_func.npz"
+    for i in {1..13}
+    do
+       str3=$str1${i}$str2
+       rm -rf str3
+    done
+    echo "Deleted files"
+fi
--- Python/IntegralEquation/Scripts/lse_helmholtz_comp_2d.py
+++ Python/IntegralEquation/Scripts/lse_helmholtz_comp_2d.py
@@ -590,4 +590,12 @@ while pml_cells < 500:
         scale=scale_sol
     )
 
+    if pml_cells == 360:
+        plot_sol(
+            x-x2,
+            "sol_diff_lse_helmholtz" + str(pml_cells) + ".pdf",
+            "Solution Difference (real)" + str(pml_cells),
+            scale=scale_sol
+        )
+
     pml_cells *= 2
